dfa56c8e3e82419a51e5e8746cfde6b2aacbfd96
sally <peacheeks@gmail.com>
2022-07-15 17:32:19 +0900

    javascript v1

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ const btnCollapse = document.getElementById('btn-collape'),
         contentPart = document.querySelector('.content-title'),
         body = document.getElementsByClassName('content-body');
 
-//To do if click the title
+//To do after click the title
+// title is not only one title[0],title[1],title[2]
+for(let i = 0; i < title.length; i++ ){ // click every title
+    title[i].addEventListener('click', function(){
+       for(let j = 0; j < contentPart.length; j++){  //click every contentPart
+         contentPart[j].classList.remove('active');  
+       } 
+    });
+}
