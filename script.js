4a6f924c9041c76b8f719d0f6ba1874eab636ec9
sally <peacheeks@gmail.com>
2022-07-15 17:26:29 +0900

    first css

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const btnCollapse = document.getElementById('btn-collape'), 
+        title = document.getElementsByClassName('content-title'),
+        contentPart = document.querySelector('.content-title'),
+        body = document.getElementsByClassName('content-body');
+
+//To do if click the title
